b9bb42e88d0b075d7a475e06f5384ef9a6d5e50a
Badri Adhikari <adkbadree@gmail.com>
2022-05-27 13:40:31 -0400

    After deleting unnecessatry Repository

--- src/Practice/Methods.java
+++ src/Practice/Methods.java
@@ -17,6 +17,8 @@ public class Methods {
     }
     public static void multiply(int a, int b){
         System.out.println(a *  b);
+        System.out.println(a+b);
+
 
     }
     public static void add(int a, int b){
